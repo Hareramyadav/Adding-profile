af15c8f8645e0c8a6f94521fc604aca583c98c7b
Hareram yadav <yhareram73@gmail.com>
2020-05-25 11:54:35 +0545

    Updates

---
+++ precache-manifest.493bcfacc8ab26eaba62ce8a030ed873.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7b605e7c2617deccc61ddc15404eeba4",
+    "url": "/react-redux/index.html"
+  },
+  {
+    "revision": "7ae3328b8fe6f63aeb81",
+    "url": "/react-redux/static/css/main.22eac29f.chunk.css"
+  },
+  {
+    "revision": "bfda130816b634361d25",
+    "url": "/react-redux/static/js/2.69501c46.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-redux/static/js/2.69501c46.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7ae3328b8fe6f63aeb81",
+    "url": "/react-redux/static/js/main.d7e324f5.chunk.js"
+  },
+  {
+    "revision": "39db6e8ea35d16f017ef",
+    "url": "/react-redux/static/js/runtime-main.9f23a8a7.js"
+  }
+]);
